6dfc0dd9cdfa69b48dbcfce28de05ce80eb0aa02
DaLei Sun <sundalei1988@gmail.com>
2022-01-28 15:47:21 +0800

    first commit

---
+++ application/src/main/java/com/example/multimodule/application/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.multimodule.application;
+
+import com.example.multimodule.service.MyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication(scanBasePackages = "com.example.multimodule")
+@RestController
+public class DemoApplication {
+
+    private final MyService myService;
+
+    public DemoApplication(MyService myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return myService.message();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
